e372c22f68a89589878a42c799085004bb2fe967
Sophia Kim <sjk7fu@virginia.edu>
2015-11-14 15:14:13 -0500

    another ballGo file

--- simulation/ballGo.py
+++ simulation/ballGo.py
@@ -4,33 +4,6 @@ import runWorld as rw
 import drawWorld as dw
 import pygame as pg
 
-################################################################
-
-# This program is an interactive simulation/game. A cat starts
-# to move across the screen. The direction of movement is reversed
-# on each "mouse down" event.
-#
-# The state of the cat is represented by a tuple (pos, delta-pos).
-# The first element, pos, represents the x-coordinate of the cat.
-# The second element, delta-pos, represents the amount that the
-# position changes on each iteration of the simulation loop.
-#
-# For example, the tuple (7,1) would represent the cat at x-coord,
-# 7, and moving to the right by 1 pixel per "clock tick."
-# 
-# The initial state of the cat in this program is (0,1), meaning that the cat
-# starts at the left of the screen and moves right one pixel per tick.
-#
-# Pressing a mouse button down while this simulation run updates the cat state
-# by leaving pos unchanged but reversing delta-pos (changing 1 to -1 and vice
-# versa). That is, pressing a mouse key reverses the direction of the
-# cat.
-#
-# The simulation ends when the cat is allowed to reach either the left
-# or the right edge of the screen.
-
-################################################################
-
 # Initialize world
 name = "Cat Fun. Press the mouse (but not too fast)!"
 width = 500
@@ -42,10 +15,6 @@ rw.newDisplay(width, height, name)
 # Display the state by drawing a cat at that x coordinate
 myimage = dw.loadImage("basketballimage.png")
 
-# state -> image (IO)
-# draw the cat halfway up the screen (height/2) and at the x
-# coordinate given by the first component of the state tuple
-#
 def updateDisplay(state):
     dw.fill(dw.black)
     dw.draw(myimage, (state[0], height/2))
@@ -64,29 +33,14 @@ def updateState(state):
 
 ################################################################
 
-# Terminate the simulation when the x coord reaches the screen edge,
-# that is, when pos is less then zero or greater than the screen width
-# state -> bool
 def endState(state):
     if (state[0] > width or state[0] < 0):
         return True
     else:
         return False
 
-
 ################################################################
 
-# We handle each event by printing (a serialized version of) it on the console
-# and by then responding to the event. If the event is not a "mouse button down
-# event" we ignore it by just returning the current state unchanged. Otherwise
-# we return a new state, with pos the same as in the original state, but
-# delta-pos reversed: if the cat was moving right, we update delta-pos so that
-# it moves left, and vice versa. Each mouse down event changes the cat
-# direction. The game is to keep the cat alive by not letting it run off the
-# edge of the screen.
-#
-# state -> event -> state
-#
 def handleEvent(state, event):  
 #    print("Handling event: " + str(event))
     if (event.type == pg.MOUSEBUTTONDOWN):
@@ -99,8 +53,6 @@ def handleEvent(state, event):
         return(state)
 
 ################################################################
-
-# World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right 
 initState = (0,1)
